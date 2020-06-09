3855d5368a4f5c0a554d1d009f50a3a45310a925
Horia123 <hori.neamtu@gmail.com>
2020-06-09 19:09:29 +0300

    Add Solution to Problem #1.
    Added solution to Problem 1
    + renamed Day1.dll to Days.dll- it will be used for all future problems. Each problem will be a sepparate class in this class library.

--- C#/DailyCodingProblems/Program.cs
+++ C#/DailyCodingProblems/Program.cs
@@ -1,20 +1,21 @@
-﻿using Day1;
+﻿using Days;
 using System;
 
 namespace DailyCodingProblems
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             Console.Write("Please input a number representing the day you want to run: ");
             int.TryParse(Console.ReadLine(), out int problemToBeRan);
 
             if (problemToBeRan == (int)DayToRunEnums.Day1)
             {
-                var anyTwoNumbersAddUp = new AnyTwoNoInArrayAddUp();
-                anyTwoNumbersAddUp.TwoNumbersInArrayAddUp();                
-            }            
+                var day1 = new Day1();
+
+                day1.TwoNumbersInArrayAddUp();
+            }
         }
     }
 }
---
+++ Day1/Day1/Day1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Days
+{
+    public class Day1
+    {
+        public void TwoNumbersInArrayAddUp()
+        {
+            Console.WriteLine("Building the data-set");
+            Console.Write("Input the No. of Elements in the array: ");
+            int.TryParse(Console.ReadLine(), out int noOfElements);
+            var arr = new List<int>();
+            bool found = false;
+
+            Console.WriteLine("Input elements: ");
+            for(var i = 0; i < noOfElements; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int elem);
+                arr.Add(elem);
+            }
+
+            Console.WriteLine("Input sum: ");
+            int.TryParse(Console.ReadLine(), out int k);
+
+            Console.Write("Finding any two numbers in the list : ");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine($"That add up to {k} ... ");
+
+            HashSet<int> values = new HashSet<int>();
+            for(int i = 0; i < arr.Count; i++)
+            {                
+                if (values.Contains(k - arr[i]))
+                    found = true;
+
+                values.Add(arr[i]);
+            }
+
+            if (found)
+            {
+                Console.WriteLine("Two such numbers were FOUND");
+            }
+            else
+            {
+                Console.WriteLine("Two such numbers were NOT FOUND");
+            }            
+        }
+    }
+}
