64350f51a71f5fdfd9b4332052dc74f33b87ba13
Horia Neamtu <horia.neamtu@uniserv.com>
2020-06-11 11:18:51 +0300

    minor improvment of Program.cs

--- C#/DailyCodingProblems/Program.cs
+++ C#/DailyCodingProblems/Program.cs
@@ -8,19 +8,24 @@ namespace DailyCodingProblems
         public static void Main()
         {
             Console.Write("Please input a number representing the day you want to run: ");
-            int.TryParse(Console.ReadLine(), out int problemToBeRan);
+            int.TryParse(Console.ReadLine(), out var problemToBeRan);
 
-            if (problemToBeRan == (int)DayToRunEnums.Day1)
+            switch (problemToBeRan)
             {
-                var day1 = new Day1();
+                case (int)DayToRunEnums.Day1:
+                {
+                    var day1 = new Day1();
 
-                day1.TwoNumbersInArrayAddUp();
-            }
-            else if (problemToBeRan == (int)DayToRunEnums.Day2)
-            {
-                var day2 = new Day2();
+                    day1.TwoNumbersInArrayAddUp();
+                    break;
+                }
+                case (int)DayToRunEnums.Day2:
+                {
+                    var day2 = new Day2();
 
-                day2.MultiplyArrElementsSkippingCurrentElement();
+                    day2.MultiplyArrElementsSkippingCurrentElement();
+                    break;
+                }
             }
         }
     }
